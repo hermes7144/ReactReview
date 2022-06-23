86fd9d1d9d7e07e62d3b06408db5089330bc30b1
hermes7144 <kichancho@gmail.com>
2022-06-24 00:34:19 +0900

    react-redux practice2

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,13 +1,15 @@
-import Counter from './components/Counter';
-import Todos from './components/Todos'
+import React from 'react';
+import CounterContainer from './container/CounterContainer';
+import Todos from './components/Todos';
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer number={0} />
+      <hr />
       <Todos />
     </div>
-  )
-}
+  );
+};
 
 export default App;
